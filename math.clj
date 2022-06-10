6df4a334d42e8e07d07c0eb0619587fc981cef0a
Székely Márton <53979153+PiciAkk@users.noreply.github.com>
2022-06-10 22:11:16 +0200

    Create math.clj

---
+++ math.clj
@@ -0,0 +1,49 @@
+(def precedence {'+ 3                 
+                 '- 3
+                 '/ 2
+                 '* 2
+                 '** 1})
+
+(defmacro $=
+  "A macro for converting infix expressions to Lisp sexps."
+  [& expr]
+  (-> (list '-> (first expr))
+      (concat (sort-by (comp precedence first)
+                       (partition 2 (rest expr))))))
+
+(defn **
+  "Returns aᵇ."
+  [a b]
+  (Math/pow a b))
+
+(defn sum
+  "Returns ∑ₓ₌ₐᵇ f(x)."
+  [a b f]
+  (apply + (map f (range a (inc b)))))
+
+(defn prod
+  "Returns ∏ₓ₌ₐᵇ f(x)."
+  [a b f]
+  (apply * (map f (range a (inc b)))))
+
+(defn !
+  "Returns n!."
+  [n]
+  (if (= n 0)
+    1
+    (* (! (dec n)) n)))
+
+(defn integrate
+  "Returns ∫ₐᵇ f(x) x*dx with the specified precision."
+  [a b f x prec]
+  (let [columns (** 10 prec)]
+    (* (/ (sum a (* b columns) #(f (* % (/ 1 columns)))) columns) x)))
+
+(defn root
+  "Returns the nth √ of x."
+  [n x]
+  (** x (/ 1 n)))
+
+(def sqrt
+  "Returns √(n)"
+  (partial root 2))
